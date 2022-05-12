991b9691f611f9862b611b9dcfa82da989eb8104
unknown <elcreidoluis@gmail.com>
2022-05-12 15:17:06 -0400

    Changes to load data

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Areas, Equipos, Materiales, Personal, Partidas, Obras
+from .models import User, Areas, Equipos, Materiales, Personal, Partidas, Obras, PrecioMateriales, PrecioEquipos, PrecioPersonal, CantidadEquipos, CantidadMateriales, CantidadPersonal, CantidadPartidas
 
 # Register your models here.
 admin.site.register(User)
@@ -9,3 +9,10 @@ admin.site.register(Materiales)
 admin.site.register(Personal)
 admin.site.register(Partidas)
 admin.site.register(Obras)
+admin.site.register(PrecioMateriales)
+admin.site.register(PrecioEquipos)
+admin.site.register(PrecioPersonal)
+admin.site.register(CantidadMateriales)
+admin.site.register(CantidadEquipos)
+admin.site.register(CantidadPersonal)
+admin.site.register(CantidadPartidas)
